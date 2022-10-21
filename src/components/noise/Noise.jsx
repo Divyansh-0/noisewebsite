dfe699181dff6973ebbf90d764289a15e94f9a99
Divyansh-0 <divyansh07mishra@gmail.com>
2022-10-21 16:26:58 +0530

    three

--- src/components/noise/Noise.jsx
+++ src/components/noise/Noise.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import "./noise.scss";
@@ -67,120 +67,137 @@ const Noise = () => {
   };
 
   return (
-    <div className="noise">
-      <button className="noise-btn">Customize the image setting</button>
-      <form className="form" onSubmit={submitHandler}>
-        <label className="label">Octaves</label>
-        <div className="field">
-          <span>{enteredOctave}</span>
-          <input
-            type={"range"}
-            className="input"
-            max="8"
-            min="1"
-            step="1"
-            onChange={OctaveChange}
-            value={enteredOctave}
-            disabled={isDisabled}
-          ></input>
-        </div>
-
-        <label className="label">Persistance</label>
-        <FontAwesomeIcon icon="fa-thin fa-circle-chevron-down" />
-        <div className="field">
-          <span>{enteredPersistance}</span>
-          <input
-            type={"range"}
-            className="input"
-            max="1"
-            min="0.00"
-            step="0.01"
-            onChange={PersistanceChange}
-            value={enteredPersistance}
-            disabled={isDisabled}
-          ></input>
-        </div>
-        <label className="label">lacunarity</label>
-        <input
-          type={"range"}
-          className="input"
-          onChange={lacunarityChange}
-          value={enteredlacunarity}
-          disabled={isDisabled}
-        ></input>
-        <label className="label">Seed</label>
-        <input
-          type={"number"}
-          className="input"
-          onChange={SeedChange}
-          value={enteredSeed}
-          disabled={isDisabled}
-        ></input>
-        <label className="label">Frequency</label>
-        <div className="field">
-          <span>{enteredFrequency}</span>
-          <input
-            type={"range"}
-            max="32"
-            min="1"
-            step="1"
-            className="input"
-            onChange={FrequencyChange}
-            value={enteredFrequency}
-            disabled={isDisabled}
-          ></input>
-        </div>
-        <label className="label">Wavelength</label>
-        <div className="field">
-          <span>{enteredWavelength}</span>
-          <input
-            type={"range"}
-            min="0"
-            step="128"
-            max="2000"
-            className="input"
-            onChange={WavelengthChange}
-            value={enteredWavelength}
-            disabled={isDisabled}
-          ></input>
-        </div>
-        {
-          // <input
-          //   type={"range"}
-          //   className="input"
-          //   onChange={WarpChange}
-          //   value={enteredWarp}
-          // ></input>
-          <div className="dropdown">
-            <div
-              className="dropdown-btn"
-              onClick={(e) => setSelected(!isSelected)}
-            >
-              Warp <FontAwesomeIcon icon="fa-thin fa-circle-chevron-down" />
-              {isSelected && (
-                <div className="dropdown-content">
-                  <div className="dropdown-item">A</div>
-                  <div className="dropdown-item">B</div>
-                  <div className="dropdown-item">C</div>
-                </div>
-              )}
-            </div>
+    <Fragment>
+      <div className="noise">
+        <span className="title">Customize Image settings</span>
+        <form className="form" onSubmit={submitHandler}>
+          <label className="label">Octaves</label>
+          <div className="field">
+            <span>{enteredOctave}</span>
+            <input
+              type={"range"}
+              className="range"
+              max="8"
+              min="1"
+              step="1"
+              onChange={OctaveChange}
+              value={enteredOctave}
+              disabled={isDisabled}
+            ></input>
           </div>
-        }
-        <label className="label">Range</label>
-        <input
-          type={"number"}
-          className="input"
-          value={enteredRange}
-          onChange={RangeChange}
-          disabled={isDisabled}
-        ></input>
-        {isDisabled && (
-          <button onClick={(e) => setIsDisabled(!isDisabled)}>Edit</button>
-        )}
-        {!isDisabled && <button type="submit">Apply Changes</button>}
-      </form>
-    </div>
+
+          <label className="label">Persistance</label>
+          <FontAwesomeIcon icon="fa-thin fa-circle-chevron-down" />
+          <div className="field">
+            <span>{enteredPersistance}</span>
+            <input
+              type={"range"}
+              className="range"
+              max="1"
+              min="0.00"
+              step="0.01"
+              onChange={PersistanceChange}
+              value={enteredPersistance}
+              disabled={isDisabled}
+            ></input>
+          </div>
+          <label className="label">Lacunarity</label>
+          <input
+            type={"range"}
+            className="range"
+            onChange={lacunarityChange}
+            value={enteredlacunarity}
+            disabled={isDisabled}
+          ></input>
+
+          <label className="label">Frequency</label>
+          <div className="field">
+            <span>{enteredFrequency}</span>
+            <input
+              type={"range"}
+              max="32"
+              min="1"
+              step="1"
+              className="range"
+              onChange={FrequencyChange}
+              value={enteredFrequency}
+              disabled={isDisabled}
+            ></input>
+          </div>
+          <label className="label">Wavelength</label>
+          <div className="field">
+            <span>{enteredWavelength}</span>
+            <input
+              type={"range"}
+              min="0"
+              step="128"
+              max="2000"
+              className="range"
+              onChange={WavelengthChange}
+              value={enteredWavelength}
+              disabled={isDisabled}
+            ></input>
+          </div>
+
+          <label className="label">Range</label>
+          <input
+            type={"number"}
+            className="input"
+            value={enteredRange}
+            onChange={RangeChange}
+            disabled={isDisabled}
+          ></input>
+          <label className="label">Seed</label>
+          <input
+            type={"number"}
+            className="input"
+            onChange={SeedChange}
+            value={enteredSeed}
+            disabled={isDisabled}
+          ></input>
+
+          {
+            <div className="dropdown">
+              <div
+                className="dropdown-btn"
+                onClick={(e) => setSelected(!isSelected)}
+              >
+                Warp <FontAwesomeIcon icon="fa-thin fa-circle-chevron-down" />
+                {isSelected && (
+                  <div className="dropdown-content">
+                    <div className="dropdown-item">
+                      A
+                      <label class="container">
+                        <input type="checkbox" />
+                        <div class="checkmark"></div>
+                      </label>
+                    </div>
+                    <div className="dropdown-item">
+                      B
+                      <label class="container">
+                        <input type="checkbox" />
+                        <div class="checkmark"></div>
+                      </label>
+                    </div>
+                    <div className="dropdown-item">
+                      C
+                      <label class="container">
+                        <input type="checkbox" />
+                        <div class="checkmark"></div>
+                      </label>
+                    </div>
+                  </div>
+                )}
+              </div>
+            </div>
+          }
+          {isDisabled && (
+            <button onClick={(e) => setIsDisabled(!isDisabled)}>Edit</button>
+          )}
+          {!isDisabled && <button type="submit">Apply Changes</button>}
+        </form>
+      </div>
+    </Fragment>
   );
 };
 
